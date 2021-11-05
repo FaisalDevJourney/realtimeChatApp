c507a9b64a407d7ef258db9fe22694c17e477e0f
byteThePizza <faisal.abuomar@outlook.com>
2021-11-05 12:38:46 +0400

    update

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,5 @@
 const Post = require("../models/Post");
-
+const User = require("../models/User");
 const router = require("express").Router();
 
 //create post
@@ -29,7 +29,59 @@ router.put("/:id", async (req,res)=>{
     }
 })
 //delete post
+
+router.delete("/:id", async (req,res)=>{
+    try{
+        const post = await Post.findById(req.params.id);
+        if(post.userId === req.body.userId){
+            await Post.findByIdAndDelete(req.params.id);
+            res.status(200).json("post deleted");
+        }
+    }catch(err){
+        res.status(403).json("you cant delete this post");
+    }
+})
 //like post
+
+router.put("/:id/like", async (req,res)=>{
+    try{
+        const post = await Post.findById(req.params.id);
+        if(!post.likes.includes(req.body.userId)){
+            await post.updateOne({$push:{likes:req.body.userId}});
+            res.status(200).json("the post has been liked");
+        }else{
+            await post.updateOne({$pull:{likes:req.body.userId}});
+            res.status(200).json("the post has been disliked");
+        }
+    }catch(err){
+        res.status(500).json("cant like this post")
+    }
+})
 //get a post
+router.get("/:id", async (req,res)=>{
+    try{
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
 //get timeline posts
+
+router.get("/timeline/all", async (req,res)=>{
+    
+    try{
+        const currentUser = await User.findById(req.body.userId);
+        const userPosts = await Post.find({userId: currentUser._id});
+        const friendPosts = await Promise.all(
+            currentUser.followings.map((friendId)=>{
+                return Post.find({userId:friendId});
+            })
+        );
+        res.json(userPosts.concat(...friendPosts));
+
+    }catch(err){
+        console.log(err);
+    }
+})
 module.exports = router;
